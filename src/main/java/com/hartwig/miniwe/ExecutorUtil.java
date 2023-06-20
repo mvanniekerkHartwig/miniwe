400bafc66e64a8f4cf89665e8591d3a740abbc31
Matthijs van Niekerk <m.vanniekerk@hartwigmedicalfoundation.nl>
2023-06-20 18:06:58 +0200

    DEV-3333 clean up code after testing

--- src/main/java/com/hartwig/miniwe/ExecutorUtil.java
+++ src/main/java/com/hartwig/miniwe/ExecutorUtil.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
-public final class ThreadUtil {
+public final class ExecutorUtil {
     public static ExecutorService createExecutorService(int nMaxThreads, String nameTemplate) {
         return new ThreadPoolExecutor(1,
                 nMaxThreads,
